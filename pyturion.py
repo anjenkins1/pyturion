dec83f6d083e72384d10e5d4919316b5bacd6ef0
sysadmin <sysadmin@sysadmins-iMac-010025118017.byu.edu>
2020-09-10 16:33:17 -0600

    Boilerplate

---
+++ pyturion.py
@@ -0,0 +1,53 @@
+"**************************************************************"
+"    ************---------PYTURION--------***************"
+"    Python tool for accessing and administering multiple "
+"         linux servers quickly and efficiently"
+"***************************************************************"
+
+#!/usr/bin/env python3
+
+"""
+Module Docstring
+"""
+
+__author__ = "Andrew Jenkins"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import argparse
+
+
+def main(args):
+    for a in args:
+        argparse.
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+
+    # Required positional argument
+    parser.add_argument("arg", help="Required positional argument")
+
+    # Optional argument flag which defaults to False
+    parser.add_argument("-f", "--flag", action="store_true", default=False)
+
+    # Optional argument which requires a parameter (eg. -d test)
+    parser.add_argument("-n", "--name", action="store", dest="name")
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Verbosity (-v, -vv, etc)")
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+
+    args = parser.parse_args()
+    main(args)
